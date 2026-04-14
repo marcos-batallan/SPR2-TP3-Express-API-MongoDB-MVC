a51b1bd7763e0639b5036e8af9ae74a766344fce
Marcos Batallan <sickness.meb@gmail.com>
2026-04-14 09:06:57 -0300

    Actualización: edad > 30, planeta Tierra y al menos 2 poderes

--- repositories/SuperHeroRepository.mjs
+++ repositories/SuperHeroRepository.mjs
@@ -7,9 +7,14 @@ import IRepository from "./IRepository.mjs";
 // Esta repo debe cumplir con el contrato de herencia de IRepository
 class SuperHeroRepository extends IRepository {
     async obtenerMayoresDe30 () {
-    // Se implementa un filtro por edad usando el operador de
-    // comparación $gt de MongoDB para traer documentos cuya edad sea mayor a 30.
-    return await SuperHero.find({ edad: { $gt: 30 } });
+        
+        return await SuperHero.find({
+            // Se utliza el operador de comparación $gt (mayor que) de MongoDB para traer documentos cuya edad sea mayor a 30.
+            edad: { $gt: 30 },
+            planetaOrigen: 'Tierra',
+            // Los arrays empiezan en índice 0. Existe un segundo elemento y por lo tanto, hay al menos 2 poderes.
+            "poderes.1": { $exists: true }
+        });
     }
     
     async buscarPorAtributo (atributo, valor) {
